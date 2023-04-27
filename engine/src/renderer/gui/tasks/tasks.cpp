a5e31ae05aeb88ef4082c79b20ab70185b576d34
quarzasiphix <quarrysiphix@gmail.com>
2023-04-27 17:57:54 +0200

    first push before adding search algorithm

--- engine/src/renderer/gui/tasks/tasks.cpp
+++ engine/src/renderer/gui/tasks/tasks.cpp
@@ -36,60 +36,59 @@ void listproc() {
 }
 
 namespace engine {
-		void tasks::getList() {
-			HANDLE hProcessSnap;
-			PROCESSENTRY32 pe32;
+	void tasks::getList() {
+		HANDLE hProcessSnap;
+		PROCESSENTRY32 pe32;
 
-			// Take a snapshot of all processes in the system.
-			hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-			if (hProcessSnap == INVALID_HANDLE_VALUE) {
-				printf("Error: CreateToolhelp32Snapshot (of processes)\n");
-				return;
-			}
-
-			// Set the size of the structure before using it.
-			pe32.dwSize = sizeof(PROCESSENTRY32);
-
-			// Retrieve information about the first process,
-			// and exit if unsuccessful
-			if (!Process32First(hProcessSnap, &pe32)) {
-				printf("Error: Process32First\n"); // Show cause of failure
-				CloseHandle(hProcessSnap);          // Clean the snapshot object
-				return;
-			}
-			// Now walk the snapshot of processes, and
-			// store information about each process in turn
-			do {
-				std::pair<std::wstring, DWORD> process(pe32.szExeFile, pe32.th32ProcessID);
-				processes.push_back(process);
-			} while (Process32Next(hProcessSnap, &pe32));
-
-			CloseHandle(hProcessSnap);
+		// Take a snapshot of all processes in the system.
+		hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+		if (hProcessSnap == INVALID_HANDLE_VALUE) {
+			printf("Error: CreateToolhelp32Snapshot (of processes)\n");
+			return;
 		}
 
-		tasks::tasks() {
-			EN_INFO("getting list of tasks");
-			getList();
-		}
+		// Set the size of the structure before using it.
+		pe32.dwSize = sizeof(PROCESSENTRY32);
 
-		std::wstring utf8_to_utf16(const std::string& str) {
-			int len = MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, nullptr, 0);
-			std::wstring wstr(len, 0);
-			MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, wstr.data(), len);
-			return wstr;
+		// Retrieve information about the first process,
+		// and exit if unsuccessful
+		if (!Process32First(hProcessSnap, &pe32)) {
+			printf("Error: Process32First\n"); // Show cause of failure
+			CloseHandle(hProcessSnap);          // Clean the snapshot object
+			return;
 		}
+		// Now walk the snapshot of processes, and
+		// store information about each process in turn
+		do {
+			std::pair<std::wstring, DWORD> process(pe32.szExeFile, pe32.th32ProcessID);
+			processes.push_back(process);
+		} while (Process32Next(hProcessSnap, &pe32));
 
-		std::string utf16_to_utf8(const std::wstring& wstr) {
-			int len = WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, nullptr, 0, nullptr, nullptr);
-			std::string str(len, 0);
-			WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, str.data(), len, nullptr, nullptr);
-			return str;
-		}
+		CloseHandle(hProcessSnap);
 
-	void tasks::list() {
-		ImGui::Begin("tasks");
-		static ImGuiTableFlags flags = ImGuiTableFlags_SizingStretchSame | ImGuiTableFlags_Resizable | ImGuiTableFlags_BordersOuter | ImGuiTableFlags_BordersV | ImGuiTableFlags_ContextMenuInBody;
-		ImGui::Text("tasks");
+		get_list = true;
+	}
+
+	tasks::tasks() {
+		EN_INFO("getting list of tasks");
+		//getList();
+	}
+
+	std::wstring utf8_to_utf16(const std::string& str) {
+		int len = MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, nullptr, 0);
+		std::wstring wstr(len, 0);
+		MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, wstr.data(), len);
+		return wstr;
+	}
+
+	std::string utf16_to_utf8(const std::wstring& wstr) {
+		int len = WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, nullptr, 0, nullptr, nullptr);
+		std::string str(len, 0);
+		WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, str.data(), len, nullptr, nullptr);
+		return str;
+	}
+
+	void tasks::fav_list() {
 		if (!favproc.empty()) {
 			if (ImGui::BeginTable("favtable", 2, flags)) {
 				ImGui::TableNextRow();
@@ -102,19 +101,18 @@ namespace engine {
 					for (int column = 0; column < 2; column++) {
 						ImGui::TableSetColumnIndex(column);
 						if (column == 0) {
-							ImGui::BeginTable("nested_table1", 2);
+							ImGui::BeginTable("name_table", 2);
 							ImGui::TableNextRow();
 							ImGui::TableSetColumnIndex(0);
-							ImGui::Text("%s", utf16_to_utf8(favproc[row].first).c_str());
-							if (ImGui::Button("remove"))
-								favproc.erase(favproc.begin() + row);
+							ImGui::Text("name: %s", utf16_to_utf8(favproc[row].first).c_str());
+							if (ImGui::Button("remove")) favproc.erase(favproc.begin() + row);
 							ImGui::EndTable();
 						}
 						else if (column == 1) {
-							ImGui::BeginTable("nested_table2", 2);
+							ImGui::BeginTable("id_table", 2);
 							ImGui::TableNextRow();
 							ImGui::TableSetColumnIndex(0);
-							ImGui::Text("id:");
+							ImGui::Text("id: ");
 							ImGui::TableSetColumnIndex(1);
 							ImGui::Text("%d", favproc[row].second);
 							ImGui::EndTable();
@@ -124,8 +122,14 @@ namespace engine {
 				ImGui::EndTable();
 			}
 		}
+	}
+
+	void tasks::all_list() {
+		static char search_term[256] = "";
 
 		if (ImGui::CollapsingHeader("tasks-list")) {
+			ImGui::InputText("Search", search_term, sizeof(search_term));
+
 			if (ImGui::BeginTable("alltable", 2, flags)) {
 				ImGui::TableNextRow();
 				ImGui::TableSetColumnIndex(0);
@@ -137,18 +141,22 @@ namespace engine {
 					for (int column = 0; column < 2; column++) {
 						ImGui::TableSetColumnIndex(column);
 						if (column == 0) {
-							ImGui::BeginTable("nested_table1", 2);
+							ImGui::BeginTable("name_table", 2);
 							ImGui::TableNextRow();
 							ImGui::TableSetColumnIndex(0);
-							ImGui::Text("%s", utf16_to_utf8(processes[row].first).c_str());
-							if (ImGui::Button("favorite")) {
+							std::string proc = std::string("name: " + utf16_to_utf8(processes[row].first));
+							if (ImGui::Button(proc.c_str())) {
 								std::pair<std::wstring, DWORD> fav(processes[row].first, processes[row].second);
 								favproc.push_back(fav);
 							}
+							if (ImGui::IsItemHovered()) {
+								ImGui::Text(std::string("add favorite: " + proc).c_str());
+							}
 							ImGui::EndTable();
 						}
+						
 						else if (column == 1) {
-							ImGui::BeginTable("nested_table2", 2);
+							ImGui::BeginTable("id_table", 2);
 							ImGui::TableNextRow();
 							ImGui::TableSetColumnIndex(0);
 							ImGui::Text("id:");
@@ -161,19 +169,20 @@ namespace engine {
 				ImGui::EndTable();
 			}
 		}
+	}
 
-		/*
-		int i = 0;
-		for (const auto& process : processes) {
-			std::string name = "name: " + utf16_to_utf8(process.first) + std::to_string(i);
-			ImGui::Text(name.c_str());
-			ImGui::Text("id: %d", process.second);
-			i++;
+	void tasks::list() {
+		ImGui::Begin("tasks");
+		
+		if (get_list == false) {
+			if (ImGui::Button("get list")) getList();
+			ImGui::End();
+			return;
 		}
-		*/
+		
+		ImGui::Text("tasks");
+		fav_list();
+		all_list();
 		ImGui::End();
 	}
-	/*void tasks::select() {
-		return;
-	}*/
 }
--- engine/src/renderer/gui/tasks/tasks.hpp
+++ engine/src/renderer/gui/tasks/tasks.hpp
@@ -5,9 +5,14 @@ namespace engine {
 	struct ENGINE_API tasks {
 		std::vector<std::pair<std::wstring, DWORD>> processes;
 		std::vector<std::pair<std::wstring, DWORD>> favproc;
+		ImGuiTableFlags flags = ImGuiTableFlags_SizingStretchSame | ImGuiTableFlags_Resizable | ImGuiTableFlags_BordersOuter | ImGuiTableFlags_BordersV | ImGuiTableFlags_ContextMenuInBody;
+
+		bool get_list = false;	
 
 		tasks();
 		void getList();
+		void fav_list();
+		void all_list();
 		void list();
 
 		//void select();
